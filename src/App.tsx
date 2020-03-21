da97f6977c6f8855b9bfd74f0d9ae043f59b8040
mustafasaifee42 <saifee.mustafa@gmail.com>
2020-03-21 07:10:27 +0200

    adding ga

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Visualization from  './Visualization';
 import { FacebookIcon, TwitterIcon } from 'react-share';
 import { FacebookShareButton, TwitterShareButton } from 'react-share';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-161398132-1');
+ReactGA.set({ anonymizeIp: true });
+ReactGA.pageview('/');
 
 function App() {
   return (
@@ -10,10 +15,10 @@ function App() {
       <div className='footer'>
         <span>Made by <a href="https://twitter.com/mustafasaifee42"  rel="noopener noreferrer" target="_blank">Mustafa Saifee</a>. Spread the information and stay safe</span>
         <div className='icons'>
-          <FacebookShareButton className="fbIcon" url={'http://coronavirus-dashboard.netlify.com'} quote={'Live dashboard visualizing the spread of coronavirus COVID-19'}>
+          <FacebookShareButton className="fbIcon" url={'https://coronavirus-dashboard.netlify.com/'} quote={'Live dashboard visualizing the spread of coronavirus COVID-19'}>
             <FacebookIcon size={24} round={true} />
           </FacebookShareButton>
-          <TwitterShareButton url={'http://coronavirus-dashboard.netlify.com'} title={'Live dashboard visualizing the spread of coronavirus COVID-19 http://coronavirus-dashboard.netlify.com via @mustafasaifee42'}>
+          <TwitterShareButton url={'https://coronavirus-dashboard.netlify.com/'} title={'Live dashboard visualizing the spread of coronavirus COVID-19 https://coronavirus-dashboard.netlify.com/ via @mustafasaifee42'}>
             <TwitterIcon size={24} round={true} />
           </TwitterShareButton>
         </div>
