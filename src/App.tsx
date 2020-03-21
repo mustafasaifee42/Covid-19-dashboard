d1ae40580d1a532b1f108efa3b07c93b4b1e7014
mustafasaifee42 <saifee.mustafa@gmail.com>
2020-03-21 06:58:18 +0200

    upadting weblink

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ function App() {
       <div className='footer'>
         <span>Made by <a href="https://twitter.com/mustafasaifee42"  rel="noopener noreferrer" target="_blank">Mustafa Saifee</a>. Spread the information and stay safe</span>
         <div className='icons'>
-          <FacebookShareButton className="fbIcon" url={'http://covid-19-dashboard.netlify.com/'} quote={'Live dashboard visualizing the spread of coronavirus COVID-19'}>
+          <FacebookShareButton className="fbIcon" url={'http://coronavirus-dashboard.netlify.com'} quote={'Live dashboard visualizing the spread of coronavirus COVID-19'}>
             <FacebookIcon size={24} round={true} />
           </FacebookShareButton>
-          <TwitterShareButton url={'http://covid-19-dashboard.netlify.com/'} title={'Live dashboard visualizing the spread of coronavirus COVID-19 http://covid-19-dashboard.netlify.com/ via @mustafasaifee42'}>
+          <TwitterShareButton url={'http://coronavirus-dashboard.netlify.com'} title={'Live dashboard visualizing the spread of coronavirus COVID-19 http://coronavirus-dashboard.netlify.com via @mustafasaifee42'}>
             <TwitterIcon size={24} round={true} />
           </TwitterShareButton>
         </div>
+        <span>This page is hosted on <a href="https://www.netlify.com/"  rel="noopener noreferrer" target="_blank">Netlify</a></span>
       </div>
     </div>
   );
